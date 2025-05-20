902e7eaa0eb413cdda6ff319455018a913e44ab2
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-20 09:57:16 +0330

    asdf

--- app/[locale]/(default)/about-us/page.tsx
+++ app/[locale]/(default)/about-us/page.tsx
@@ -5,6 +5,9 @@ import AboutUsImage from "@/public/images/about2.jpg";
 import MissionAndGoal from "@/public/images/mission-and-goal.png";
 import { getAboutUs } from "@/actions/about-us";
 import BossMessage from "@/components/about-us/boss-message";
+import { Divider } from "@heroui/divider";
+
+
 // import secondBossMessage from "@/components/about-us/secondBoss";
 import AboutUsCards from "@/components/about-us/about-use-cards";
 
@@ -29,7 +32,7 @@ export default async function AboutPage({ params }: { params: any }) {
         <BossMessage />
 
         {/* <secondBossMessage /> */}
-        
+
 
         <AboutUsCards />
         <div className="mt-20 flex flex-col md:flex-row justify-around max-w-screen-lg">
@@ -53,8 +56,58 @@ export default async function AboutPage({ params }: { params: any }) {
             </p>
           </div>
         </div>
+
+        <Divider className = "my-4"/>
+
         <div className="flex flex-col justify-center items-center my-10 max-w-screen-md">
           <Title page="aboutUs" titleText="Mission and goals" />
+
+          <ol className="font-light text-center text-justify leading-6 mt-4 px-2 max-w-3xl"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            >
+            <li>
+              <span class="highlight">تقویت و گسترش شبکه سمن‌های فعال:</span> ایجاد و تقویت شبکه‌ای گسترده از سازمان‌های مردم‌نهاد (NGOs) فعال
+              در حوزه میراث فرهنگی ناملموس در آسیای غربی و مرکزی، به منظور تسهیل ارتباطات و اشتراک‌گذاری دانش و
+              تجربیات.
+            </li>
+
+            <li>
+              <span class="highlight">اطلاع‌رسانی در خصوص نقش سمن‌ها در کنوانسیون 2003:</span> افزایش آگاهی سمن‌ها در مورد اهمیت و نقش آن‌ها
+              در اجرای کنوانسیون 2003 یونسکو، و ترویج بهترین شیوه‌های حفاظت از میراث فرهنگی ناملموس.
+            </li>
+
+            <li>
+              <span class="highlight">ایجاد پایگاه داده از سمن‌ها:</span> شناسایی، مستندسازی و نقشه‌برداری از سازمان‌های مردم‌نهاد فعال در منطقه برای
+              تسهیل ارتباطات، برنامه‌ریزی مشترک و تبادل اطلاعات.
+            </li>
+
+            <li>
+              <span class="highlight">گسترش همکاری‌های بین‌المللی:</span> تقویت همکاری‌های فرامرزی میان سمن‌ها برای به اشتراک‌گذاری دانش، منابع
+              و تجربیات، و ایجاد پل‌های فرهنگی برای تقویت هویت‌های بومی.
+            </li>
+
+            <li>
+              <span class="highlight">پاسداری از میراث فرهنگی ناملموس:</span> حمایت از تلاش‌های منطقه‌ای برای حفظ و ترویج میراث فرهنگی ناملموس،
+              با تأکید بر اهمیت این میراث در تقویت هویت فرهنگی.
+            </li>
+
+            <li>
+              <span class="highlight">ظرفیت‌سازی و آموزش:</span> توانمندسازی سمن‌ها از طریق ارائه دوره‌های آموزشی، کارگاه‌های تخصصی و ارتقاء
+              ظرفیت‌های بومی برای حفاظت مؤثر از میراث فرهنگی.
+            </li>
+
+            <li>
+              <span class="highlight">ارتقاء آگاهی عمومی:</span> افزایش آگاهی عمومی در مورد اهمیت میراث فرهنگی ناملموس و نقش آن در توسعه پایدار
+              جوامع محلی.
+            </li>
+
+            <li>
+              <span class="highlight">ترویج تحقیقات و مستندسازی:</span> تقویت فعالیت‌های پژوهشی و مستندسازی به منظور انتقال بهتر دانش فرهنگی به
+              نسل‌های آینده.
+            </li>
+          </ol>
+
           {/* <Image
             alt="mission and goal"
             className="md:w-1/3 w-2/3"
@@ -62,7 +115,7 @@ export default async function AboutPage({ params }: { params: any }) {
             data-aos-duration="1000"
             src={MissionAndGoal}
           /> */}
-          <p
+          {/* <p
             className="font-light text-center text-justify leading-6 mt-4 px-2 max-w-3xl"
             data-aos="fade-up"
             data-aos-duration="1000"
@@ -72,7 +125,7 @@ export default async function AboutPage({ params }: { params: any }) {
               : locale === "en"
                 ? data?.enMissionAndGoals
                 : data?.ruMissionAndGoals}
-          </p>
+          </p> */}
         </div>
       </section>
     </>
